dc954c6ef8ff4e1fc31ba5cf6ec3cd1f611fed6c
GOSSIPCOIN <dev@gossipcoin.net>
2020-07-27 22:29:28 +0200

    again typo

--- mn_A-E_installer.sh
+++ mn_A-E_installer.sh
@@ -71,7 +71,7 @@ function bare() {
   docker volume create --name "$mn"
   docker pull smai/"$mn"_be_phantom:0.0.1
   docker run -d --restart always -v "$mn":/root/phantom/conf:ro --name "$mn"-backend smai/"$mn"_be_phantom:0.0.1
-  docker pull smai/"$mn"_phantom:0.0.1
+  docker pull smai/"$mn"_fe_phantom:0.0.1
   docker run -d --restart always -p 8080:8080 -v "$mn":/root/phantom-hosting/conf --name "$mn"-frontend smai/"$mn"_fe_phantom:0.0.1
   ufw allow 8084/tcp comment ""$MN" GUI" >/dev/null
   echo "alias "$mn"-conf='cd /var/lib/docker/volumes/"$mn"/_data/'" >> ~/.bash_aliases
